fec2ba1d6b175efb2d1d4b635cffbcbaa30c63f4
mbyford49 <mbyford49@gmail.com>
2024-04-19 17:26:44 -0700

    started questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,42 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
 
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+const path = require('path');
 // TODO: Create an array of questions for user input
-const questions = [];
+
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your project title?',
+        name: 'title',
+    },
+
+    {
+        type: 'input',
+        message: 'Provide a description of your project',
+        name: 'description',
+    },
+
+    {
+        type: 'input',
+        message: 'How is this application installed?',
+        name: 'installation',
+    },
+
+    {
+        type: 'input',
+        message: 'How is this application used?',
+        name: 'usage',
+    },
+
+    {
+        type: 'input',
+        message: 'How is this application tested?',
+        name: 'testing',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
