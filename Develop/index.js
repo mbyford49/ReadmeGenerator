f434cddbdb1022abcdf6c882b7188cc986be6d42
mbyford49 <mbyford49@gmail.com>
2024-04-22 15:26:56 -0700

    finish questions array

--- Develop/index.js
+++ Develop/index.js
@@ -36,6 +36,25 @@ const questions = [
         message: 'How is this application tested?',
         name: 'testing',
     },
+
+    {
+        type: 'list',
+        message: 'Choose a license',
+        choices: ['MIT','Apache 2.0','BSD 3-Clause New or Revised','BSD 2-Clause Simplified','ISC','Creative Commons Zero v1.0 Universal','GNU Lesser General Public v3.0','Mozilla Public 2.0'],
+        name: 'license',
+    },
+
+    {
+        type: 'input',
+        message: 'Enter email address',
+        name: 'email',
+    },
+
+    {
+        type: 'input',
+        message: 'Enter Github username',
+        name: 'github',
+    },
 ];
 
 // TODO: Create a function to write README file
